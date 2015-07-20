3fd65d07678e250a7c629e0f44111e4215ee360e
Christopher Paxton <cpaxton3@jhu.edu>
2015-07-20 17:46:07 -0400

    added another demonstration to test a few things

--- scripts/segment.py
+++ scripts/segment.py
@@ -18,49 +18,68 @@ if __name__ == "__main__":
     rospy.init_node('segment_demonstrations')
 
     if len(sys.argv) > 1:
-        filename = sys.argv[1]
+        filenames = sys.argv[1:len(sys.argv)]
     else:
-        filename = "demo.yml"
+        filenames = ["demo.yml"]
 
     rate = rospy.Rate(10)
 
-    stream = file(filename,'r');
-    demo = yaml.load(stream,Loader=Loader);
+    dbg_ee_poses = PoseArray()
+    dbg_ee_poses2 = PoseArray()
+    dbg_ee = PoseArray()
+    dbg_link = PoseArray()
+    dbg_node = PoseArray()
+    dbg_ee_poses.header.frame_id = "/gbeam_link_1/gbeam_link"
+    dbg_ee_poses2.header.frame_id = "/gbeam_node_1/gbeam_node"
+    dbg_ee.header.frame_id = "/world"
+    dbg_link.header.frame_id = "/world"
+    dbg_node.header.frame_id = "/world"
 
-    print "Loaded data, computing features..."
-    fx,x,u,t = demo.get_features([('ee','link'),('ee','node'),('link','node')])
+    for filename in filenames:
 
-    print "Done computing features. %d total time steps."%(len(fx))
-    print "Segmenting data based on gripper commands..."
+        stream = file(filename,'r');
+        demo = yaml.load(stream,Loader=Loader);
 
-    gripping = abs(fx[0][0]) > 0
-    action = 0;
-    labels = [];
-    for i in range(len(t)):
-        if fx[i][0] > 0 and not gripping:
-            action += 1
-            gripping = True
-        elif not fx[i][0] > 0 and gripping:
-            action += 1
-            gripping = False
-        labels += [action]
+        print "Loaded data from '%s', computing features..."%(filename)
+        fx,x,u,t = demo.get_features([('ee','link'),('ee','node'),('link','node')])
 
-    print labels
+        print "Done computing features. %d total time steps."%(len(fx))
+        print "Segmenting data based on gripper commands..."
 
-    [lfx, lx, lu] = GetSegment(fx,x,u,labels,0)
+        gripping = abs(fx[0][0]) > 0
+        action = 0;
+        labels = [];
+        for i in range(len(t)):
+            if fx[i][0] > 0 and not gripping:
+                action += 1
+                gripping = True
+            elif not fx[i][0] > 0 and gripping:
+                action += 1
+                gripping = False
+            labels += [action]
 
-    print "%d samples selected for segment"%(len(lfx))
+        print labels
 
-    #these are definitely wrong
-    #dbg_ee_poses = GetPoseMessage(fx,4,'/wam/hand/bhand_palm_link') #"/gbeam_link_1/gbeam_link")
-    #dbg_ee_poses2 = GetPoseMessage(fx,12,'/wam/hand/bhand_palm_link')#"/gbeam_node_1/gbeam_node")
+        [lfx, lx, lu] = GetSegment(fx,x,u,labels,0)
 
-    dbg_ee_poses = GetPoseMessage(fx,4,"/gbeam_link_1/gbeam_link")
-    dbg_ee_poses2 = GetPoseMessage(fx,11,"/gbeam_node_1/gbeam_node")
-    dbg_ee = demo.get_world_pose_msg("ee")
-    dbg_link = demo.get_world_pose_msg("link")
-    dbg_node = demo.get_world_pose_msg("node")
-    
+        print "%d samples selected for segment"%(len(lfx))
+
+        #these are definitely wrong
+        #dbg_ee_poses = GetPoseMessage(fx,4,'/wam/hand/bhand_palm_link') #"/gbeam_link_1/gbeam_link")
+        #dbg_ee_poses2 = GetPoseMessage(fx,12,'/wam/hand/bhand_palm_link')#"/gbeam_node_1/gbeam_node")
+
+        dbg_ee_poses_ = GetPoseMessage(fx,4,"/gbeam_link_1/gbeam_link")
+        dbg_ee_poses2_ = GetPoseMessage(fx,11,"/gbeam_node_1/gbeam_node")
+        dbg_ee_ = demo.get_world_pose_msg("ee")
+        dbg_link_ = demo.get_world_pose_msg("link")
+        dbg_node_ = demo.get_world_pose_msg("node")
+
+        dbg_ee_poses.poses += dbg_ee_poses_.poses
+        dbg_ee_poses2.poses += dbg_ee_poses2_.poses
+        dbg_ee.poses += dbg_ee_.poses
+        dbg_link.poses += dbg_link_.poses
+        dbg_node.poses += dbg_node_.poses
+        
     pa_pub = rospy.Publisher('/dbg_ee_link',PoseArray)
     pa2_pub = rospy.Publisher('/dbg_ee_node',PoseArray)
     pa_ee_pub = rospy.Publisher('/dbg_ee',PoseArray)
