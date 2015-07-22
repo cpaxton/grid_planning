94c216789199bd96b6836feea9a7dc17b00cd133
Christopher Paxton <cpaxton3@jhu.edu>
2015-07-22 14:10:18 -0400

    plotting data/hdp-hmm tests

--- scripts/plot_data.py
+++ scripts/plot_data.py
@@ -2,8 +2,16 @@ import grid
 import matplotlib.pyplot as plt
 import numpy as np
 
+'''
+PLOT_DATA
+This script goes through some data, shows some python plots, and experiments a bit with HDP-HSMM clustering.
+The idea is that we can use this sort of clustering to detect repeated segments.
+Each segment 
+'''
+
+frames=[('ee','link'),('ee','node')]
 data = grid.LoadYaml('demo3.yml')
-fx,x,u,t = data.get_features([('ee','link'),('ee','node')])
+fx,x,u,t = data.get_features(frames)
 ndata = np.array(fx)
 
 xx = np.array(range(ndata.shape[0]))
@@ -16,3 +24,59 @@ for i in range(ndata.shape[1]):
 #plt.plot(xx,yy)
 
 plt.show()
+
+'''
+This section does some simple HDP-HSMM clustering.
+Features are Gaussian-distributed and durations are Poisson distributed.
+'''
+
+print "clustering..."
+
+import copy
+import pyhsmm
+import pyhsmm.basic.distributions as distributions
+from pybasicbayes.util.text import progprint_xrange
+
+obs_dim = ndata.shape[1]
+Nmax = 25
+
+obs_hypparams = {'mu_0':np.zeros(obs_dim),
+                'sigma_0':np.eye(obs_dim),
+                'kappa_0':0.3,
+                'nu_0':obs_dim+5}
+dur_hypparams = {'alpha_0':2*30,
+                 'beta_0':2}
+
+obs_distns = [distributions.Gaussian(**obs_hypparams) for state in range(Nmax)]
+dur_distns = [distributions.PoissonDuration(**dur_hypparams) for state in range(Nmax)]
+
+#posteriormodel = pyhsmm.models.WeakLimitHDPHSMM(
+posteriormodel = pyhsmm.models.WeakLimitHDPHMM(
+        alpha=6.,gamma=6., # better to sample over these; see concentration-resampling.py
+        init_state_concentration=6., # pretty inconsequential
+        obs_distns=obs_distns)#,
+        #dur_distns=dur_distns)
+
+
+data2 = grid.LoadYaml('demo2.yml')
+fx,x,u,t = data2.get_features(frames)
+ndata2 = np.array(fx)
+data1 = grid.LoadYaml('demo1.yml')
+fx,x,u,t = data1.get_features(frames)
+ndata1 = np.array(fx)
+
+#posteriormodel.add_data(ndata,trunc=60)
+#posteriormodel.add_data(ndata1,trunc=60)
+#posteriormodel.add_data(ndata2,trunc=60)
+
+posteriormodel.add_data(ndata)
+posteriormodel.add_data(ndata1)
+posteriormodel.add_data(ndata2)
+
+models = []
+for idx in progprint_xrange(150):
+    posteriormodel.resample_model()
+    if (idx+1) % 10 == 0:
+        models.append(copy.deepcopy(posteriormodel))
+
+
