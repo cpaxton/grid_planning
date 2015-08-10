389e2c6b990e55a05be41305c68ead8239349d67
Christopher Paxton <cpaxton3@jhu.edu>
2015-08-10 17:11:08 -0400

    adding a CMakeLists file that was missing

--- python/grid/skill.py
+++ python/grid/skill.py
@@ -1,5 +1,40 @@
 
+# ROS stuff
+import rospy
+from urdf_parser_py.urdf import URDF
+import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+import copy
+
+import numpy as np
+
+# KDL utilities
+import PyKDL
+from pykdl_utils.kdl_kinematics import KDLKinematics
+from pykdl_utils.kdl_parser import kdl_tree_from_urdf_model
+
+# machine learning utils (python)
 from sklearn.mixture import GMM
+
+
+# tf stuff
+import tf
+import tf_conversions.posemath as pm
+
+# input message types 
+import sensor_msgs
+import oro_barrett_msgs
+import trajectory_msgs
+from trajectory_msgs.msg import JointTrajectoryPoint
+from sensor_msgs.msg import JointState
+from oro_barrett_msgs.msg import BHandCmd
+
+# output message types
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import PoseArray
 
 
 '''
