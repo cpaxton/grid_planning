fixing normalization issues

python/grid/features.py
@@ -151,6 +151,10 @@ class RobotFeatures:
         self.action_std = None
         self.goal_mean = None
         self.goal_std = None
+        self.action_mean_ng = None
+        self.action_std_ng = None
+        self.goal_mean_ng = None
+        self.goal_std_ng = None
 
         self.recorded = False
         self.quiet = True # by default hide TF error messages
@@ -416,8 +420,9 @@ class RobotFeatures:
 
         features,goal_features = self.GetFeaturesForTrajectory(traj,world,objs)
 
-        features = self.NormalizeAction(features)
-        goal_features = self.NormalizeGoal(goal_features)
+        features = self.NormalizeActionNG(features)
+        if not self.goal_model is None:
+            goal_features = self.NormalizeGoalNG(goal_features)
 
         N = features.shape[0]
         #print N,features.shape
@@ -521,16 +526,23 @@ class RobotFeatures:
                 obj_frame = world[obj]
 
                 # ... so get object offset to end effector ...
-                offset = (obj_frame*PyKDL.Frame(PyKDL.Rotation.RotY(np.pi/2))).Inverse() * (self.base_tform * ee_frame)
+                #offset = (obj_frame*PyKDL.Frame(PyKDL.Rotation.RotY(np.pi/2))).Inverse() * (self.base_tform * ee_frame)
+                offset = obj_frame.Inverse() * (self.base_tform * ee_frame)
 
                 # ... use position offset and distance ...
                 features += offset.p
                 features += [offset.p.Norm()]
 
                 # ... and use axis/angle representation
-                (theta,w) = offset.M.GetRotAngle()
-                features += [theta*w[0],theta*w[1],theta*w[2],theta]
-                #features += [w[0],w[1],w[2],theta]
+                #(theta,w) = offset.M.GetRotAngle()
+                #features += [theta*w[0],theta*w[1],theta*w[2],theta]
+                #(theta,w) = offset.M.GetRotAngle()
+                (x,y,z,w) = offset.M.GetQuaternion()
+                #print offset.M.GetQuaternion()
+                #print PyKDL.Rotation.Quaternion(x,y,z,w).GetQuaternion()
+                #print '----'
+                #raw_input()
+                features += [x,y,z,w]
 
         return features
 
@@ -647,10 +659,16 @@ class RobotFeatures:
     def SetActionNormalizer(self,skill):
         self.action_mean = skill.action_mean
         self.action_std = skill.action_std
+        self.action_mean_ng = skill.action_mean_ng
+        self.action_std_ng = skill.action_std_ng
     def NormalizeAction(self,features):
         return (features - self.action_mean) / self.action_std
+    def NormalizeActionNG(self,features):
+        return (features - self.action_mean_ng) / self.action_std_ng
     def DenormalizeAction(self,features):
         return (features * self.action_std) + self.action_mean
+    def DenormalizeActionNG(self,features):
+        return (features * self.action_std_ng) + self.action_mean_ng
 
     '''
     Set up mean/std to normalize incoming goal data
@@ -658,10 +676,16 @@ class RobotFeatures:
     def SetGoalNormalizer(self,skill):
         self.goal_mean = skill.goal_mean
         self.goal_std = skill.goal_std
+        self.goal_mean_ng = skill.goal_mean_ng
+        self.goal_std_ng = skill.goal_std_ng
     def NormalizeGoal(self,features):
         return (features - self.goal_mean) / self.goal_std
+    def NormalizeGoalNG(self,features):
+        return (features - self.goal_mean_ng) / self.goal_std_ng
     def DenormalizeGoal(self,features):
         return (features * self.goal_std) + self.goal_mean
+    def DenormalizeGoalNG(self,features):
+        return (features * self.goal_std_ng) + self.goal_mean_ng
 
 def LoadRobotFeatures(filename):
 

python/grid/grid_types.py
@@ -113,8 +113,11 @@ def GetPoseMessage(fx, idx, frame_id="/world"):
     for i in range(len(fx)):
 
         pose = PyKDL.Vector(fx[i][idx],fx[i][idx+1],fx[i][idx+2]);
-        rot = PyKDL.Rotation.RPY(fx[i][idx+3],fx[i][idx+4],fx[i][idx+5])
-        frame = PyKDL.Frame(rot,pose) * PyKDL.Frame(PyKDL.Rotation.RotY(-1*np.pi/2))
+        #rot = PyKDL.Rotation.RPY(fx[i][idx+3],fx[i][idx+4],fx[i][idx+5])
+        q = np.array([fx[i][idx+3],fx[i][idx+4],fx[i][idx+5],fx[i][idx+6]])
+        q = q / np.linalg.norm(q)
+        rot = PyKDL.Rotation.Quaternion(q[0],q[1],q[2],q[3])
+        frame = PyKDL.Frame(rot,pose) * PyKDL.Frame(PyKDL.Rotation.RotZ(-1*np.pi/2))
         pmsg = pm.toMsg(frame)
         msg.poses.append(pmsg)
 

python/grid/skill.py
@@ -69,10 +69,10 @@ class RobotSkill:
         if filename == None and len(data) > 0:
 
             ''' compute means and normalize incoming data '''
-            self.action_mean = np.mean(data)
-            self.action_std = np.std(data)
-            self.goal_mean = np.mean(data)
-            self.goal_std = np.std(data)
+            self.action_mean = np.mean(data,axis=0)
+            self.action_std = np.std(data,axis=0)
+            self.goal_mean = np.mean(data,axis=0)
+            self.goal_std = np.std(data,axis=0)
             goals = (goals - self.goal_mean) / self.goal_std
             data = (data - self.action_mean) / self.action_std
 
@@ -83,13 +83,13 @@ class RobotSkill:
             else:
                 self.goal_model.means_ = np.array([np.mean(goals,axis=0)])
                 self.goal_model.covars_ = np.array([np.cov(goals,rowvar=False)])
-                self.goal_model.covars_[0] += 1e-5 * np.eye(self.goal_model.covars_.shape[1])
+                self.goal_model.covars_[0] += 1e-10 * np.eye(self.goal_model.covars_.shape[1])
             if action_k > 1:# or True:
                 self.action_model.fit(data)
             else:
                 self.action_model.means_ = np.array([np.mean(data,axis=0)])
                 self.action_model.covars_ = np.array([np.cov(data,rowvar=False)])
-                self.action_model.covars_[0] += 1e-5 * np.eye(self.action_model.covars_.shape[1])
+                self.action_model.covars_[0] += 1e-10 * np.eye(self.action_model.covars_.shape[1])
 
             self.trajectory_model.fit(params)
             self.t_factor = 0.1
@@ -104,6 +104,11 @@ class RobotSkill:
                 self.action_model.covars_ = self.gripper_model.covars_[:,:-num_gripper_vars,:-num_gripper_vars]
                 self.goal_model.covars_ = self.goal_model.covars_[:,:-num_gripper_vars,:-num_gripper_vars]
                 self.goal_model.means_ = self.goal_model.means_[:,:-num_gripper_vars]
+
+                self.action_mean_ng = self.action_mean[:-num_gripper_vars]
+                self.action_std_ng = self.action_std[:-num_gripper_vars]
+                self.goal_mean_ng = self.goal_mean[:-num_gripper_vars]
+                self.goal_std_ng = self.goal_std[:-num_gripper_vars]
 
         elif not filename == None:
             stream = file(filename,'r')
@@ -121,6 +126,10 @@ class RobotSkill:
             self.action_std = data['action_std']
             self.goal_mean = data['goal_mean']
             self.goal_std = data['goal_std']
+            self.action_mean_ng = data['action_mean_ng']
+            self.action_std_ng = data['action_std_ng']
+            self.goal_mean_ng = data['goal_mean_ng']
+            self.goal_std_ng = data['goal_std_ng']
             self.t_factor = 0.1
 
     def GetGoalModel(self,objs):
@@ -169,6 +178,10 @@ class RobotSkill:
         out['action_std'] = self.action_std
         out['goal_mean'] = self.goal_mean
         out['goal_std'] = self.goal_std
+        out['action_mean_ng'] = self.action_mean_ng
+        out['action_std_ng'] = self.action_std_ng
+        out['goal_mean_ng'] = self.goal_mean_ng
+        out['goal_std_ng'] = self.goal_std_ng
 
         yaml.dump(out,stream)
 