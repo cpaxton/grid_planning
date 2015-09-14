e0dafe3b7d51c46109d7e4698705f6f16bbacc9c
Kel Guerin <futureneer@gmail.com>
2015-09-14 16:50:09 -0400

    terrible terrible error handling to load GripperRegressor and grid_plan even when oro barrett msgs doesn't exist

--- python/grid_plan/__init__.py
+++ python/grid_plan/__init__.py
@@ -7,6 +7,9 @@ except ImportError, e:
     print "[GRID_PLAN] Could not bind Boost::python bindings!"
 
 from planning_node import *
-from gripper_regression_node import *
+try:
+    from gripper_regression_node import *
+except ImportError, e:
+    print "[GRID_PLAN] Could not import GripperRegressor: %s"%(e.message)
 from trajectory_commander import *
 from primitives import *
