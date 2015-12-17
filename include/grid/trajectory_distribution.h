2bc7b71f60c55f7e4a1ec6d0f91823a2c351e711
Chris Paxton <cpaxton3@jhu.edu>
2015-12-17 17:12:14 -0500

    setting up more of the trajectory distribution code

--- include/grid/trajectory_distribution.h
+++ include/grid/trajectory_distribution.h
@@ -2,11 +2,38 @@
 #define _GRID_TRAJECTORY_DISTRIBUTION
 
 #include <grid/features.h>
+#include <grid/test_features.h>
 #include <grid/skill.h>
+#include <grid/dist/gmm.h>
 
 namespace grid {
 
+  class TrajectoryDistribution {
+    protected:
+      gcop::Gmm<> dist; // stores distributions
+      unsigned int nseg; // number of segments
+      unsigned int dim; // dimensionality of the trajectory space
 
+    public:
+
+      /**
+       * Initialize a trajectory distribution with velocity profile, etc.
+       */
+      TrajectoryDistribution(int nseg, int k = 1);
+
+      /**
+       * initialize
+       * set up the distribution based on a skill and an environment
+       */
+      void initialize(const Features &features, const Skill &skill);
+
+      /**
+       * sample
+       * Pull a random trajectory from the gmm
+       * Convert it into a KDL trajectory
+       */
+      Trajectory *sample() const;
+  };
 
 }
 
--- src/trajectory_distribution.cpp
+++ src/trajectory_distribution.cpp
@@ -2,5 +2,30 @@
 
 namespace grid {
 
+  /**
+   * Initialize a trajectory distribution with velocity profile, etc.
+   */
+  TrajectoryDistribution::TrajectoryDistribution(int nseg_, int k_) : nseg(nseg_), dist(nseg_*13) {
+    dim = 13 * nseg; // velocity, acceleration, position setpoints for each segment
+  }
+
+  /**
+   * initialize
+   * set up the distribution based on a skill and an environment
+   */
+  void TrajectoryDistribution::initialize(const Features &features, const Skill &skill) {
+
+  }
+
+  /**
+   * sample
+   * Pull a random trajectory from the gmm
+   * Convert it into a KDL trajectory
+   */
+  Trajectory *TrajectoryDistribution::sample() const {
+    Trajectory *traj;
+
+    return traj;
+  }
 
 }
