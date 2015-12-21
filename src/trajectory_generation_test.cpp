6599fab23d8b67dea2872d9eee638c4593e35d70
Chris Paxton <cpaxton3@jhu.edu>
2015-12-21 11:14:03 -0500

    setting up ros node for trajectory generation test

--- src/trajectory_generation_test.cpp
+++ src/trajectory_generation_test.cpp
@@ -2,9 +2,13 @@
 #include <grid/trajectory_distribution.h>
 #include <grid/test_features.h>
 
+using namespace grid;
+using namespace KDL;
 
 int main(int argc, char **argv) {
-
+  ros::init_node(argc,argv,"grid_trajectory_test_node");
+  ros::NodeHandle nh;
+  ros::Publisher pub = nh.advertise<geometry_msgs::PoseArray>("trajectory",1000);
 
 
 }
