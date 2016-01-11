d596cc17b6fccdcae05f7cbad1aa736974cd2a73
Chris Paxton <cpaxton3@jhu.edu>
2016-01-11 16:41:02 -0500

    task model helper functions added

--- include/grid/task_model.h
+++ include/grid/task_model.h
@@ -37,6 +37,9 @@ namespace grid {
 
   /**
    * Defines the whole task model.
+   * We add a set of "raw" skills, appropriately configured.
+   * We use these to set up transitions.
+   * Each skill is instantiated based on the possible assignments of objects to it.
    */
   class TaskModel {
 
@@ -47,10 +50,35 @@ namespace grid {
      */
     TaskModel();
 
+    /**
+     * add an object with a particular class
+     */
+    TaskModel &addObject(const std::string &obj, const std::string &obj_class);
+
+    /**
+     * Enable transitions between different skills
+     * This add is only in one direction from skill1 to skill2
+     */
+    TaskModel &setTransition(const Skill &skill1, const Skill &skill2, bool allowed = true);
+
+    /**
+     * add an uninstantiated skill
+     * appends it to skills vector
+     * adds a reference in skills to id mapping
+     */
+    TaskModel &addSkill(const Skill &skill);
+
   protected:
 
+    std::vector<std::string> available_objects; // unique object ids for the current world
+    std::unordered_map<std::string,std::string> object_classes; // mapping of objects to classes
+
     std::vector<double> initial; // initial skill distribution
-    std::vector<InstantiatedSkill> skills; // stores all possible actions we could take in this world
+    std::vector<InstantiatedSkill> inst_skills; // stores all possible actions we could take in this world
+
+    std::vector<Skill> skills; // all possible skills
+    std::unordered_map<std::string,unsigned int> skill_to_id; // map skills to unique ids
+    std::vector<std::vector<bool> > transitions; // allowed transition matrix
 
   };
 
--- src/task_model.cpp
+++ src/task_model.cpp
@@ -9,4 +9,50 @@ namespace grid {
 
   }
 
+  /**
+   * add an object with a particular class
+   */
+  TaskModel &TaskModel::addObject(const std::string &obj, const std::string &obj_class) {
+
+    std::cout << __FILE__ << ":" << __LINE__ << ": Not yet implemented!" << std::endl;
+
+    return *this;
+  }
+
+  /**
+   * Enable transitions between different skills
+   * This add is only in one direction from skill1 to skill2
+   */
+  TaskModel &TaskModel::setTransition(const Skill &skill1, const Skill &skill2, bool allowed) {
+
+    if (transitions.size() < skills.size()) {
+      transitions.resize(skills.size());
+    }
+    for (auto &vec: transitions) {
+      if (vec.size() < skills.size()) {
+        vec.resize(skills.size());
+      }
+    }
+
+    unsigned int idx1 = skill_to_id[skill1.getName()];
+    unsigned int idx2 = skill_to_id[skill2.getName()];
+
+    transitions[idx1][idx2] = allowed;
+
+    return *this;
+  }
+
+  /**
+   * add an uninstantiated skill
+   * appends it to skills vector
+   * adds a reference in skills to id mapping
+   */
+  TaskModel &TaskModel::addSkill(const Skill &skill) {
+    if (skill_to_id.find(skill.getName()) != skill_to_id.end()) {
+      skill_to_id[skill.getName()] = skills.size();
+      skills.push_back(skill);
+    }
+    return *this;
+  }
+
 }
