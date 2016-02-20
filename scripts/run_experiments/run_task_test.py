9f3b959b067eb104ac4d44549b4ef489d196238b
Chris Paxton <cpaxton3@jhu.edu>
2016-02-20 18:09:08 -0500

    experiment script

--- scripts/run_experiments/run_task_test.py
+++ scripts/run_experiments/run_task_test.py
@@ -1,15 +1,62 @@
 #!/usr/bin/env python
 
 import rospy
-from subprocess import call
+import tf
+from subprocess import Popen
 
-for i in range(1,11):
-    name = 'double%d:=true'%(i)
-    print name
+procs = []
 
-    launch_cmd = ['roslaunch','grid_experiments','ascent_experiments.launch',name]
+test_cmd = ["rosrun","grid_plan","task_test"]
+reset_cmd = ["rosrun","grid_experiments","reset.py"]
 
-    proc = call(launch_cmd)
+args = ["_step_size:=0.5",
+        "_iter:=15",
+        "_ntrajs:=200",
+        "_starting_horizon:=5",
+        "_max_horizon:=5",
+        "_detect_collisions:=true",
+        "_wait:=0",
+        "_collisions_verbose:=0",
+        "_base_model_norm:=0.0001",
+        "_model_norm_step:=1",
+        "_update_horizon:=0.0001"
+        ]
 
-    rospy.sleep(2.0)
-    
+try:
+    for i in range(1,11):
+
+        name = 'double%d:=true'%(i)
+        print name
+
+        launch_cmd = ['roslaunch','grid_experiments','ascent_experiments.launch',name]
+        #launch_cmd = "roslaunch grid_experimetns ascent_experiments.launch double%d:=true"%(i)
+        #print launch_cmd
+
+        #proc = call(launch_cmd)
+        proc = Popen(launch_cmd)
+        procs.append(proc)
+
+        rospy.sleep(2.0)
+
+        print "[%d] Resetting to standard start configuration..."%(i)
+        reset_proc = Popen(reset_cmd)
+        
+
+        print "[%d] Starting test..."%(i)
+        test_proc = Popen(test_cmd+args)
+
+        test_proc.wait()
+
+        print "[%d] Done test!"%(i)
+
+        break
+
+except Exception, e:
+    print e
+finally:
+    for proc in procs:
+        proc.poll()
+        if proc.returncode is None:
+            print " - Killing %s"%(str(proc))
+            proc.terminate()
+
