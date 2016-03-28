26f1141c9a7e10f21de550090827b88b343e2bae
Chris Paxton <cpaxton3@jhu.edu>
2016-03-28 14:59:03 -0400

    setting up new objects

--- grid_server/src/grid_server_node.cpp
+++ grid_server/src/grid_server_node.cpp
@@ -8,11 +8,22 @@
 #include <grid/utils/params.h>
 #include <grid/wam/input.h>
 
+#include <grid_plan_msgs/ActionConfig.h>
+#include <grid_plan_msgs/ObjectConfig.h>
+#include <grid_plan_msgs/TaskPlanRequest.h>
+
 using namespace grid;
+
+bool handleActionSequence(grid_plan_msgs::TaskPlanRequestRequest &req,
+                          grid_plan_msgs::TaskPlanRequestResponse &res)
+{
+
+
+}
 
 int main(int argc, char **argv) {
 
-
+  ros::init(argc, argv, "grid_plan_server");
 
 
   return 0;
